fa8961b1661ddc2468924b9945a2694030677373
Mehrab Rahman <mehrab.rahman@revature.com>
2019-10-22 11:48:32 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os/exec"
+)
+
+func main() {
+	/*
+		path := os.Getenv("PATH")
+		fmt.Println(path)
+
+		hostname, err := os.Hostname()
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Println(hostname)
+
+		cmd := exec.Command("ls", "-l")
+		output, stderr := cmd.Output()
+		if stderr != nil {
+			fmt.Println(stderr)
+		}
+		fmt.Println(string(output))
+	*/
+	dockerstop := exec.Command("docker", "stop", "exampledb")
+	dockerstop.Run()
+	dockerdelete := exec.Command("docker", "container", "rm", "exampledb")
+	dockerdelete.Run()
+	dockerrm := exec.Command("docker", "rmi", "exampledb")
+	dockerrm.Run()
+	dockerbuild := exec.Command("docker", "build", "-t", "exampledb", ".")
+	dockerbuild.Run()
+	dockerrun := exec.Command("docker", "run", "--name=exampledb", "-p=5432:5432", "-d", "exampledb")
+	dockerrun.Run()
+}
